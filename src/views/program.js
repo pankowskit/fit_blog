8af380379081b472a304131aff6b9b12d36128a9
tomaszpankowski <--get-all>
2022-05-23 08:31:51 +0200

    layout

--- src/views/program.js
+++ src/views/program.js
@@ -1,11 +1,19 @@
-import {Container} from "react-bootstrap";
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+import ProgramS1 from "../components/program-s1";
+import ProgramS2 from "../components/program-s2";
+import ProgramS3 from "../components/program-s3";
 
-function Program(props){
-    return(
-        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
-            
-        </Container>
-    );
+class Program extends Component{
+    render(){
+        return(        
+            <Container fluid className="minh-100vh p-0">
+                <ProgramS1/>
+                <ProgramS2/>
+                <ProgramS3/>
+            </Container>    
+        );
+    }
 }
 
 export default Program;
